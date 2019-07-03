853f4b17b82b7ce84a2e815ae4d9f37ee5c4747d
Mahfuzur Rahman <limon3640@gmail.com>
2019-07-03 12:54:30 +0600

    configure dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { EventsGateway } from './events/events.gateway';
 import { EventsModule } from './events/events.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [EventsModule, AuthModule],
+  imports: [EventsModule, AuthModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService, EventsGateway],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from './config/config.service';
 
 @Injectable()
 export class AppService {
+  private appName: string;
+  constructor(config: ConfigService) {
+    this.appName = config.get('APP_NAME')
+  }
   getHello() {
-    return 'Hello world!';
+    return 'Hello world! ' + this.appName;
   }
 }
