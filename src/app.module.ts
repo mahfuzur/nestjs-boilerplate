2d244c12bdebfb2bdfca7a1ec2d0e3a4e0e9c831
Mahfuzur Rahman <limon3640@gmail.com>
2019-07-05 15:17:24 +0600

    add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,15 @@ import { EventsGateway } from './events/events.gateway';
 import { EventsModule } from './events/events.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from './config/config.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [EventsModule, AuthModule, ConfigModule],
+  imports: [
+    EventsModule,
+    AuthModule,
+    ConfigModule,
+    MongooseModule.forRoot('mongodb://localhost/insta_api')
+  ],
   controllers: [AppController],
   providers: [AppService, EventsGateway],
 })
