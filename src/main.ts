5593b793c8ff1b88486c94cd7ec75f28d5bee987
Mahfuzur Rahman <limon3640@gmail.com>
2019-07-06 13:37:19 +0600

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ApplicationModule } from './app.module';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
+
+  const options = new DocumentBuilder()
+    .setBasePath('localhost:3000')
+    .setTitle('Insta API')
+    .setDescription('Insta API description')
+    .setVersion('1.0')
+    // .addTag('cats12')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 
   if (module.hot) {
